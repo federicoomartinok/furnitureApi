e781b905aedc925e9185dca62324b77f682d09d3
Federico Martin <federicoomartinok@gmail.com>
2023-05-29 13:32:16 -0300

    Furniture Store, added controllers

---
+++ API.FurnitureStore/API.FurnitureStore.API/Controllers/ClientsController.cs
@@ -0,0 +1,75 @@
+﻿using API.FurnitureStore.Data;
+using API.FurnitureStore.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.FurnitureStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        //Dependencia del context, dbcontext
+        private readonly APIFurnitureStoreContext _context;
+
+        //Constructor de context para injectar la dependencia
+        public ClientsController(APIFurnitureStoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        //Se crea el endpoint asincrono porque puede llegar a demorar
+        //cuando quiere ingresar a la base de datos
+        public async Task<IEnumerable<Client>> Get()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+        //Si no encuentra el cliente con firstordefault devuelve = null
+        //con single o default devuelve un error
+        //IActionResult permite devolver respuestas http responses
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDetails (int id)
+        {
+            var client = await _context.Clients.FirstOrDefaultAsync(c => c.Id == id);
+            //not found es un http response devuelve un 404
+            if (client == null) return NotFound();
+
+            return Ok(client);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Client client)
+        {
+
+            await _context.Clients.AddAsync(client);
+            await _context.SaveChangesAsync();
+
+            //Produce un 201 que es un CREATED
+            return CreatedAtAction("Post", client.Id, client);
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> Put(Client client)
+        {
+            _context.Clients.Update(client);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+
+        public async Task<IActionResult> Delete(Client client)
+        {
+            if (client == null) return NotFound();
+
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ API.FurnitureStore/API.FurnitureStore.Data/APIFurnitureStoreContext.cs
@@ -0,0 +1,33 @@
+﻿using API.FurnitureStore.Shared;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.FurnitureStore.Data
+{
+    public class APIFurnitureStoreContext : DbContext
+    {
+        public APIFurnitureStoreContext(DbContextOptions options) : base(options) { }
+        //tablas
+        public DbSet<Client> Clients { get; set; }
+        public DbSet <Product> Products { get; set; }
+        public DbSet <Order> Orders { get; set; }
+        public DbSet <ProductCategory> ProductCategories { get; set; }
+        public DbSet <OrderDetail> OrderDetails { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite();
+        }
+
+        //Luego del metodo se hace Add-Migration "Lo que quieras"
+        //Update-Database para updatear
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //Estoy estableciendo manuelmente que orderDetail tiene la clave de orderid y productId
+            modelBuilder.Entity<OrderDetail>()
+                .HasKey(od => new {od.OrderId, od.ProductId});
+        }
+    }
+}
