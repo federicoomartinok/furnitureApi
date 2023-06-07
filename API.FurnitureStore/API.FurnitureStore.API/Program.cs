f5a928ac7b5344e087c4c9e85956cbe1184a9067
Federico Martin <federicoomartinok@gmail.com>
2023-06-07 12:34:36 -0300

    JWT Tokken

--- API.FurnitureStore/API.FurnitureStore.API/Program.cs
+++ API.FurnitureStore/API.FurnitureStore.API/Program.cs
@@ -1,5 +1,9 @@
+using API.FurnitureStore.API.Configuration;
 using API.FurnitureStore.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +17,32 @@ builder.Services.AddDbContext<APIFurnitureStoreContext>(options =>
             options.UseSqlite(builder.Configuration.GetConnectionString("APIFurnitureStoreContext")));
 
 
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    //se addean las opciones del jwt
+}).AddJwtBearer(jwt =>
+{
+    var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtConfig:Secret").Value);
+    jwt.SaveToken = true;
+    jwt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        //siempre valida que la key que figura en el token
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,//en entorno prod esto tiene que ser verdadero
+        ValidateAudience = false,//esto tambien tiene que ser validado en entorno prod
+        RequireExpirationTime = false,
+        ValidateLifetime = true // tiempo de vida del token
+    };
+});
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +54,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- API.FurnitureStore/API.FurnitureStore.API/obj/Debug/net6.0/API.FurnitureStore.API.AssemblyInfo.cs
+++ API.FurnitureStore/API.FurnitureStore.API/obj/Debug/net6.0/API.FurnitureStore.API.AssemblyInfo.cs
@@ -11,6 +11,8 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("719c8c69-cb33-4981-b7f5-6c90be2fe4bc")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("API.FurnitureStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
